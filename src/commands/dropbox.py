cf93590da673d09d83fac4711282f595af6f5173
MURAOKA Yusuke <yusuke@jbking.org>
2011-11-19 22:42:42 +0900

    add reading account info from a ini file in dropbox directory.

--- src/commands/dropbox.py
+++ src/commands/dropbox.py
@@ -1,3 +1,5 @@
+from ConfigParser import ConfigParser
+from StringIO import StringIO
 from dropbox_utils import traverse
 
 
@@ -18,8 +20,9 @@ def ls_rec(client, path):
 
 def obtain_file(client, path):
     res = client.get_file(path)
-    res.name = path
-    return res
+    file_obj = StringIO(res.read())
+    file_obj.name = path
+    return file_obj
 
 
 def put_file(client, path, file_obj, overwrite=True):
@@ -28,3 +31,11 @@ def put_file(client, path, file_obj, overwrite=True):
 
 def delete_file(client, path):
     return client.file_delete(path)
+
+
+def load_netprint_account_info(client, path='/account.ini'):
+    config = ConfigParser()
+    config.readfp(obtain_file(client, path))
+    return (config.get('netprint', 'username'),
+            config.get('netprint', 'password'))
+
