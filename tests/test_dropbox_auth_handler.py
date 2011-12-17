2d18df33f4157423c387c1f86ca9c1f1a726e561
MURAOKA Yusuke <yusuke@jbking.org>
2011-12-17 15:01:11 +0900

    add a test for auth handlers

---
+++ tests/test_dropbox_auth_handler.py
@@ -0,0 +1,72 @@
+from unittest import TestCase
+from urlparse import urlparse
+
+from nose.plugins.attrib import attr
+from minimock import mock, Mock, restore
+
+from test_utils import create_user
+
+
+class AuthorizeHandlerTest(TestCase):
+    url = "http://fake.host/faked_url"
+
+    def setUp(self):
+        from google.appengine.ext.testbed import Testbed
+
+        self.testbed = Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+
+        # mockout
+        import netprintbox.service
+        mock('netprintbox.service.DropboxService.build_authorize_url',
+             returns=self.url)
+
+    def tearDown(self):
+        restore()
+
+    def _getAUT(self):
+        from main import app
+        return app
+
+    @attr('functional', 'light')
+    def test_it(self):
+        app = self._getAUT()
+        response = app.get_response('/dropbox/authorize')
+        self.assertEqual(response.status_int, 302)
+        self.assertEqual(response.location, self.url)
+
+
+class AuthorizeCallbackHandlerTest(TestCase):
+    oauth_token = 'fake_token'
+
+    def setUp(self):
+        from google.appengine.ext.testbed import Testbed
+
+        self.testbed = Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+
+        self.user = create_user()
+
+        # mockout
+        import netprintbox.service
+        mock('netprintbox.service.DropboxService.setup_user',
+             returns=self.user)
+
+    def tearDown(self):
+        restore()
+
+    def _getAUT(self):
+        from main import app
+        return app
+
+    @attr('functional', 'light')
+    def test_it(self):
+        app = self._getAUT()
+        response = app.get_response('/dropbox/callback?oauth_token=hoge')
+        self.assertEqual(response.status_int, 302)
+
+        result = urlparse(response.location)
+        self.assertEqual(result.path, '/guide/setup')
+        self.assertEqual(result.query, 'key=%s' % self.user.access_key)
