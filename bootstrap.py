397a136eb2ef5ae187fc133ab984a8ef84829aa1
MURAOKA Yusuke <yusuke@jbking.org>
2011-12-19 19:17:41 +0900

    split main to bootstrap and actual main

---
+++ bootstrap.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.path.insert(0, 'bundle')
+sys.path.insert(0, 'src')
+
+from netprintbox import main
+
+app = main.app
--- src/netprintbox/main.py
+++ src/netprintbox/main.py
@@ -1,7 +1,3 @@
-import sys
-sys.path.insert(0, 'bundle')
-sys.path.insert(0, 'src')
-
 import logging
 
 import webapp2
