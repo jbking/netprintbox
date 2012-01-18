d3c16b03bdf92a53768fb45fafb6d4228ac85b52
MURAOKA Yusuke <yusuke@jbking.org>
2012-01-18 18:11:56 +0900

    split dropbox transaction out

--- src/netprintbox/transaction.py
+++ src/netprintbox/transaction.py
@@ -16,9 +16,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import logging
+import uuid
 
-from dateutil.parser import parse
-from google.appengine.ext import db
+from dateutil.parser import parse as dt_parse
 
 from netprintbox.data import FileState, DropboxFileInfo
 from netprintbox.utils import normalize_name, is_generated_file
@@ -66,7 +66,7 @@ class SyncTransaction(object):
             path = dropbox_item['path']
             size = dropbox_item['bytes']
             rev = dropbox_item['rev']
-            modified = parse(dropbox_item['modified'])
+            modified = dt_parse(dropbox_item['modified'])
             netprint_id = netprint_item['id']
 
             logging.debug(u"File exists on both: %s(%s)", path, rev)
@@ -127,7 +127,7 @@ class SyncTransaction(object):
             path = dropbox_item['path']
             size = dropbox_item['bytes']
             rev = dropbox_item['rev']
-            modified = parse(dropbox_item['modified'])
+            modified = dt_parse(dropbox_item['modified'])
 
             logging.debug(u"File exists on dropbox: %s(%s)", path, rev)
 
@@ -237,3 +237,117 @@ class SyncTransaction(object):
 
         for key in key_in_gae - (key_in_dropbox | key_in_netprint):
             self._gae_only(info_in_gae[key])
+
+
+class TransactionBase(object):
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def uid(self):
+        if getattr(self, '_uid_cache', None) is None:
+            self._uid_cache = str(uuid.uuid4())
+        return self._uid_cache
+
+    @uid.setter
+    def uid(self, value):
+        self._uid_cache = value
+
+    def run(self):
+        raise NotImplementedError
+
+
+class DropboxTransaction(TransactionBase):
+    ACCOUNT_CAPACITY = 10 * 1024 * 1024  # 10 MB
+
+    def __init__(self, context):
+        super(DropboxTransaction, self).__init__(context)
+        self.target = context.user.own_files()
+        self.capacity = self.ACCOUNT_CAPACITY \
+                - sum(obj.size for obj in self.target)
+
+    def _incr_capacity(self, size):
+        self.capacity += size
+
+    def _decr_capacity(self, size):
+        if self.capacity - size < 0:
+            raise OverLimit("Up to account capacity by %dbytes." % size)
+        self.capacity -= size
+
+    def run(self):
+        entries_on_dropbox = self._collect_entries_on_dropbox()
+        entries_on_site = self._collect_entries_on_site()
+
+        keys_on_site = set(entries_on_site)
+        keys_on_dropbox = set(entries_on_dropbox)
+        only_exists_on_site = keys_on_site - keys_on_dropbox
+        only_exists_on_dropbox = keys_on_dropbox - keys_on_site
+        exists_on_both = keys_on_site & keys_on_dropbox
+
+        # In order of possibility of deleting.
+        for k in only_exists_on_site:
+            self._run_for_item_only_on_site(entries_on_site[k])
+
+        for k in exists_on_both:
+            self._run_for_item_on_both(entries_on_site[k],
+                                       entries_on_dropbox[k])
+
+        for k in only_exists_on_dropbox:
+            self._run_for_item_only_on_dropbox(entries_on_dropbox[k])
+
+    def _run_for_item_only_on_site(self, item):
+        """
+        Recognize as gone file.
+        """
+        self._incr_capacity(self, item['bytes'])
+        file_info = self.target.filter('path = ', item['path']).get()
+        file_info.delete()
+
+    def _run_for_item_only_on_dropbox(self, item):
+        """
+        Recognize as new file.
+        """
+        self._decr_capacity(item['bytes'])
+        file_info = DropboxFileInfo(parent=self.context.user,
+                                    path=item['path'],
+                                    rev=item['rev'],
+                                    size=item['bytes'],
+                                    state=FileState.NEED_NETPRINT_ID,
+                                    last_modified=dt_parse(item['modified']))
+        file_info.put()
+
+    def _run_for_item_on_both(self, item_on_site, item_on_dropbox):
+        rev = item_on_dropbox['rev']
+        size = item_on_dropbox['bytes']
+        file_info = self.target.filter('path = ', item_on_site['path']).get()
+        if file_info.rev != rev:
+            # Updated file.
+            self._decr_capacity(size - file_info.size)
+            file_info.rev = rev
+            file_info.size = size
+            file_info.state = FileState.NEED_NETPRINT_ID
+            file_info.last_modified = dt_parse(item_on_dropbox['modified'])
+            file_info.put()
+
+    def _collect_entries_on_site(self):
+        result = {}
+        for file_info in self.target:
+            result[file_info.path] = {
+                    'path': file_info.path,
+                    'rev': file_info.rev,
+                    'bytes': file_info.size,
+                }
+        return result
+
+    def _collect_entries_on_dropbox(self):
+        data = self.context.dropbox.list('/')
+        result = {}
+
+        def __collect_entries(data):
+            if not (data['is_dir']
+                    or data.get('is_deleted', False)
+                    or is_generated_file(data['path'])):
+                result[data['path']] = data
+
+        traverse(__collect_entries, data)
+        return result
